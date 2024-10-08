ef913cb66f531f9f1669622fb71d0adc961f684e
zack v <zvcoding@gmail.com>
2024-10-08 16:06:59 -0400

    24

---
+++ app/campaign/result/page.js
@@ -0,0 +1,8 @@
+// app/campaign/result/page.js
+import ClientResult from './ClientResult';
+
+export const dynamic = 'force-dynamic';
+
+export default function ResultPage() {
+  return <ClientResult />;
+}
